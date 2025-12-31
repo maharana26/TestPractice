10d97af950e47a44ead31105e26b56dfdcb48bae
maharana26 <maharanasmrutika@gmail.com>
2025-12-31 19:07:47 +0530

    Area

--- TestVariables.js
+++ TestVariables.js
@@ -1,3 +1,47 @@
+//Addition
 let a = 10
 let b = 4
 console.log(a+b)
+
+//substraction
+let c = 8
+let d = 10
+console.log(c-d)
+
+//Multiplication
+let e = 40
+let f = 2
+console.log(e*f)
+
+//Division
+let g = 100
+let t = 2
+console.log(g/t)
+
+function calculate(x,y) {
+    console.log("Addition:", x+y);
+    console.log("Substraction:", x-y);
+    console.log("Multiplication:", x*y);
+    console.log("Division:", x/y);
+}
+calculate(10,5);
+
+
+//Area of a circle
+const pi = 3.14;
+const radius = 10;
+const area = (("Area of a circle:") + pi*radius*radius);
+console.log(area);
+  
+//Area of a Cylinder
+const p = 3.14;
+const r = 5;
+const height = 2;
+const areaofCylinder = (("Area of a cylinder:") + 2*p*r*height);
+console.log(areaofCylinder);
+
+//Area of a Rectagle
+const h = 10;
+const w = 5;
+const areaofRectangle = (("Area of a rectangle:") + h*w)
+console.log(areaofRectangle);
