0010cb224f8a6ecc7ee232f7175de871cee4fae5
maharana26 <maharanasmrutika@gmail.com>
2026-01-02 10:59:04 +0530

    calculation

---
+++ Problem1.js
@@ -0,0 +1,13 @@
+//Hike calculation 
+const ramesh = 10000;
+const suresh = 15000;
+const hike = 25;
+
+const rameshafterHike = ramesh + (ramesh * hike / 100);
+const sureshafterHike = suresh + (suresh * hike / 100);
+
+const totalSalary = ("Total salary after 25% hike: " + (rameshafterHike + sureshafterHike));
+
+console.log(rameshafterHike);
+console.log(sureshafterHike);
+console.log(totalSalary);
--- TestVariables.js
+++ TestVariables.js
@@ -45,3 +45,17 @@ const h = 10;
 const w = 5;
 const areaofRectangle = (("Area of a rectangle:") + h*w)
 console.log(areaofRectangle);
+
+//Hike calculation 
+const ramesh = 10000;
+const suresh = 15000;
+const hike = 25;
+
+const rameshafterHike = ramesh + (ramesh * hike / 100);
+const sureshafterHike = suresh + (suresh * hike / 100);
+
+const totalSalary = ("Total salary after 25% hike: " + (rameshafterHike + sureshafterHike));
+
+console.log(rameshafterHike);
+console.log(sureshafterHike);
+console.log(totalSalary);
